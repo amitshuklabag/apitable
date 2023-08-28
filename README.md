2cfd8e4e44d9af3db9c52bd90532fa76a7926089
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2023-08-28 12:36:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Apitable CI/CD pipeline
+
+<a href="https://dash.elest.io/deploy?source=cicd&social=dockerCompose&url=https://github.com/elestio-examples/apitable"><img src="deploy-on-elestio.png" alt="Deploy on Elest.io" width="180px" /></a>
+
+Deploy Apitable with CI/CD on Elestio
+
+<img src="apitable.png" style='width: 100%;'/>
+<br/>
+<br/>
+
+# Once deployed ...
+
+You can open Apitable UI here:
+
+    URL: https://[CI_CD_DOMAIN]
+    email: [ADMIN_EMAIL]
+    password: [ADMIN_PASSWORD]
+
+You can open PHPMyAdmin here:
+
+    URL: https://[CI_CD_DOMAIN]:8543
+    email: [ADMIN_EMAIL]
+    password: [ADMIN_PASSWORD]
---
+++ scripts/postInstall.sh
@@ -0,0 +1,27 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+#wait until the server is ready
+echo "Waiting for software to be ready ..."
+echo "It will take a bit of time ..."
+sleep 90s;
+
+docker-compose down;
+docker-compose up -d;
+
+echo "Waiting for software to be ready ..."
+sleep 180s;
+
+target=$(docker-compose port gateway 80)
+
+curl http://${target}/api/v1/register \
+  -H 'accept: application/json, text/plain, */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'pragma: no-cache' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36' \
+  -H 'x-front-version: v0.99.1-beta+1631.5934942576.2bf3684b35e428c284be680a05872cd1ba5aa615' \
+  -H 'x-xsrf-token: 78a3f04f-dfdd-47a9-a3bc-a1d4ef3b5bb1' \
+  --data-raw '{"username":"'${ADMIN_EMAIL}'","credential":"'${ADMIN_PASSWORD}'"}' \
+  --compressed
