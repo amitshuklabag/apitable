50d6c65773334f25546a862f1edab3f8027b491d
Amit Shukla <53058928+amitshuklabag@users.noreply.github.com>
2023-08-28 18:12:50 +0530

    Update postInstall.sh

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,13 +3,6 @@ set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-echo "It will take a bit of time ..."
-sleep 90s;
-
-docker-compose down;
-docker-compose up -d;
-
-echo "Waiting for software to be ready ..."
 sleep 180s;
 
 target=$(docker-compose port gateway 80)
